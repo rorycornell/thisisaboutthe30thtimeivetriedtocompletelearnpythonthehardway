d0efa29296c5572128a0cb9dca3728ae136181e8
rory <rory@rorys-MBP.home>
2016-05-01 16:37:36 -0400

    This is a warmup

---
+++ ex5.py
@@ -0,0 +1,8 @@
+name = "Rory Cornell"
+age = 22.4
+print "My Name is %s and I am %G years old" % (name, age)
+
+
+inches = 74
+feet = inches / 12
+print feet, "\n" + "there are %G inches in a foot." % 12
